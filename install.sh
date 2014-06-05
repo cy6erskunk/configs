26e02503157efb3a92556919b12915534d03b6ca
Igor Shevchenko <cyberskunk@gmail.com>
2014-06-05 23:00:26 +0400

    install.sh now creates "/usr/local/bin/sublime" symlink to subl to open sublime from terminal

--- install.sh
+++ install.sh
@@ -67,4 +67,12 @@ echo Done
 echo ===Getting git-prompt...
 git submodule init
 git submodule update
+
+#install sublime->subl link to open sublime from terminal
+SUBLIME="/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl"
+SUBLIME_BIN="/usr/local/bin/sublime"
+if [ -e "${SUBLIME}" ] && [ -x "${SUBLIME}" ] && [ ! -e "${SUBLIME_BIN}" ]; then
+    ln -s "${SUBLIME}" "${SUBLIME_BIN}"
+fi
+
 echo Done
