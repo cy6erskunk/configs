385203bb06c0f1a01fe881b18a8ae02b9346bc8a
Igor Shevchenko <cyberskunk@gmail.com>
2014-12-09 20:11:28 +0300

    fix typo in install.sh

--- install.sh
+++ install.sh
@@ -68,7 +68,7 @@ echo ===Getting git-prompt...
 git submodule init
 git submodule update
 
-[[ -r ./subl.sh ]] && . /subl.sh
+[[ -r ./subl.sh ]] && . ./subl.sh
 
 if [ -e $HOME/.bash_profile ]; then
     echo .bash_profile already exists, try addding \'. .bashrc\' to its contents
