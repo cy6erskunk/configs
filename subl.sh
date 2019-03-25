cfd140185adff93d4967ffd04386f7a35e784f7e
Igor Shevchenko <cyberskunk@gmail.com>
2019-03-25 21:57:56 +0200

    Remove sublime text related stuff

--- subl.sh
+++
@@ -1,6 +0,0 @@
-#install sublime->subl link to open sublime from terminal
-SUBLIME="/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl"
-SUBLIME_BIN="/usr/local/bin/sublime"
-if [ -e "${SUBLIME}" ] && [ -x "${SUBLIME}" ] && [ ! -e "${SUBLIME_BIN}" ]; then
-    ln -s "${SUBLIME}" "${SUBLIME_BIN}"
-fi
